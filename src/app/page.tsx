e9c95b97d73045f7ba51f1cf631c4edaf81960d6
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-03-22 23:04:00 -0300

    Implementação do componente LandingContent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-row items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-poppins)]">
+    <div className="justify-items-center min-h-screen gap-16 font-[family-name:var(--font-poppins)]">
       <Navbar />
       <LandingContent />
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer>
         Footer
       </footer>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full py-8 px-32">
+    <nav className="fixed top-0 w-full py-6 px-32 bg-white/90">
       <ul className="flex space-x-4 w-300 place-content-between align-middle m-auto">
         <li>
             <Link href="/" className="text-indigo-500 text-xl">
@@ -10,7 +10,7 @@ const Navbar = () => {
             </Link>
         </li>
         <li>
-            <Link href="/"className='text-lg'>Cadastrar Endereço</Link>
+            <Link href="/" className="text-base/7">Cadastrar Endereço</Link>
         </li>
       </ul>
     </nav>
